8a1cbf5ef936209111ffda6c5ae05227c92b072f
aliba <baherami@gmail.com>
2018-02-01 21:45:58 +0200

    add userprofile model to django admin

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+#import the model files
+from . import models
+
 # Register your models here.
+
+#register the user profile model
+admin.site.register(models.UserProfile)
